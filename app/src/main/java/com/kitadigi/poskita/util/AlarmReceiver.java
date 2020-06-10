0a9c21dc13db26d4d49a5e2e6ef3f561747caf2d
prima <parmi24@gmail.com>
2020-06-10 05:58:13 -0400

    dfs

--- app/src/main/java/com/kitadigi/poskita/util/AlarmReceiver.java
+++ app/src/main/java/com/kitadigi/poskita/util/AlarmReceiver.java
@@ -4,8 +4,11 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.kitadigi.poskita.R;
@@ -16,10 +19,20 @@ public class AlarmReceiver extends BroadcastReceiver implements ISinkronizer {
 
     Calendar calendar = Calendar.getInstance();
     Sinkronizer sinkronizer;
+    RemoteViews remoteViews;
 
+    String selesai;
+
+    NotificationManager mNotificationManager;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        selesai= context.getResources().getString(R.string.proses_sinkron_telah_selesai);
+
+        //siapkan layout untuk notif
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.notifikasi_sinkron_small);
+
 
         //coba buat notifikasi
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
@@ -27,15 +40,28 @@ public class AlarmReceiver extends BroadcastReceiver implements ISinkronizer {
         //pasang icon pada notifikasi
         mBuilder.setSmallIcon(R.drawable.ic_shopping_cart);
 
+
+        //pasang icon large/besar
+        Bitmap icon = BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.ic_shopping_cart);
+        mBuilder.setLargeIcon(icon);
+
+
+
+        //pasang layout buatan sendiri
+        mBuilder.setCustomContentView(remoteViews);
+        mBuilder.setStyle(new NotificationCompat.DecoratedCustomViewStyle());
+
         //pasang title
-        mBuilder.setContentTitle(context.getResources().getString(R.string.kitadigi_poskita));
+//        mBuilder.setContentTitle(context.getResources().getString(R.string.kitadigi_poskita));
 
         //pasang subtitle
-        mBuilder.setContentText(context.getResources().getString(R.string.proses_sinkron_sedang_diproses));
+//        mBuilder.setContentText(context.getResources().getString(R.string.proses_sinkron_sedang_diproses));
 
         //mulai create notifikasi dan tampilkan di HP
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(0, mBuilder.build());
+
 
         Log.d(calendar.getTime().toString(),"Proses sinkronisasi sedang berjalan...");
 
@@ -58,5 +84,9 @@ public class AlarmReceiver extends BroadcastReceiver implements ISinkronizer {
     @Override
     public void onFinish(String pesan) {
         Log.d("on finish", "FINISH");
+        remoteViews.setTextViewText(R.id.tv_text, selesai );
+
+        //hilangkan notifikasi dari hp user
+//        mNotificationManager.cancel(0);
     }
 }
